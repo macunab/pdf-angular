b149c1b35b246dde3cffbf030321cda9f480fcbc
Marshall <mn.acunab@gmail.com>
2022-12-15 16:11:28 -0300

    initial configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DialogModule } from 'primeng/dialog';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    CardModule,
+    DialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
